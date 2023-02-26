60a6d52d079d414539f59749e978846385749d91
jamesfs912 <113399123+jamesfs912@users.noreply.github.com>
2023-02-26 15:30:37 -0500

    Add files via upload
    Ok since I am programming the MCU I wrote some python programs for sine, triangle, square, sawtooth waveforms and a hand drawn user defined waveform. When you run the specific .py file the output producesa csv file that can be opened in excel and viewed as a graph. Most waveforms accept csv files so I plan on using this in the future. You could technically draw the waveform, save the csv file onto a usb drive and insert it into one of GMU's waveform generators to produce the custom signal. The GUI is taken from matlab. When you run the program a popup window should appear.

---
+++ wave drawer/square/square_wave.py
@@ -0,0 +1,22 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+
+# Generate a sequence of time values with a time step of 0.01 seconds
+t = np.arange(0, 1, 0.01)
+
+# Generate a square wave with a frequency of 1 Hz and a duty cycle of 50%
+y = np.where(np.mod(t, 1) < 0.5, 1, -1)
+
+# Write the square wave data to a CSV file
+with open('square_wave.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for i in range(len(t)):
+        writer.writerow([y[i]])
+
+# Plot the data as a graph
+plt.plot(t, y)
+plt.title('Square Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.show()
---
+++ wave drawer/test.py
@@ -0,0 +1,15 @@
+from wave_drawer import WaveDrawer
+import tkinter as tk
+
+
+# Create a tkinter window
+root = tk.Tk()
+
+# Set the window title
+root.title('Wave Drawer')
+
+# Create a WaveDrawer
+drawer = WaveDrawer(root)
+
+# Start the tkinter event loop
+root.mainloop()
