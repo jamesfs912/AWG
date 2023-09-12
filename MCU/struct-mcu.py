ad44f5d604d54a3a4e4344098a13ed96e44c1e13
jamesfs912 <113399123+jamesfs912@users.noreply.github.com>
2023-09-12 13:49:49 -0400

    Update struct-mcu.py

--- MCU/struct-mcu.py
+++ MCU/struct-mcu.py
@@ -1,14 +1,11 @@
-# my_module.py
-
 import serial
 import struct
 import time
+import math
 
-
-def create_packet1(sample_size=30, arr_value=1600):
+def create_packet1(sample_size, arr_value):
     packet1_id = 0x0101
     return struct.pack('<HHH', packet1_id, sample_size, arr_value)
-
 
 def create_packet2(lut_values):
     packet2_id = 0x0202
@@ -17,9 +14,9 @@ def create_packet2(lut_values):
         packet2_data += struct.pack('<H', val)
     return packet2_data
 
-
 def send_and_receive_data(serial_port, data):
     serial_port.write(data)
+    serial_port.flush()
     response = serial_port.read(2)
 
     if len(response) == 2:
@@ -28,33 +25,75 @@ def send_and_receive_data(serial_port, data):
     else:
         return None
 
+def get_user_input():
+    try:
+        frequency = float(input("Enter desired frequency (Hz): "))
+        num_samples = int(input("Enter number of samples: "))
+        return frequency, num_samples
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return None, None
+
+def calculate_arr_value(frequency, num_samples):
+    try:
+        arr_value = int(48000000 / (frequency * num_samples))
+        return arr_value
+    except ZeroDivisionError:
+        print("Error: Frequency and/or number of samples cannot be zero.")
+        return None
+
+def generate_sine_wave_lut(num_samples):
+    try:
+        lut_values = []
+        for i in range(num_samples):
+            angle = 2 * math.pi * i / num_samples
+            sine_value = int((math.sin(angle) + 1) * 2047)  # Scale to 0-4095
+            lut_values.append(sine_value)
+        return lut_values
+    except ZeroDivisionError:
+        print("Error: Number of samples cannot be zero.")
+        return None
 
 def main():
     ser = serial.Serial('COM9', 115200)
+    ser.close()
+    ser.open()
 
-    packet1_data = create_packet1()
-    time.sleep(1)
-    response1 = send_and_receive_data(ser, packet1_data)
-    if response1 is not None:
-        print("Received response data:", response1)
-    else:
-        print("Received invalid response")
+    try:
+        frequency, num_samples = get_user_input()
+        if frequency is None or num_samples is None:
+            return
 
-    lut_values = [
-        2048, 2473, 2880, 3251, 3569, 3821, 3995, 4084,
-        4084, 3995, 3821, 3569, 3251, 2880, 2473, 2048,
-        1622, 1215, 844, 526, 274, 100, 11, 11,
-        100, 274, 526, 844, 1215, 1622, 204
-    ]
-    packet2_data = create_packet2(lut_values)
-    time.sleep(1)
-    response2 = send_and_receive_data(ser, packet2_data)
-    if response2 is not None:
-        print("Received response data:", response2)
-    else:
-        print("Received invalid response")
+        arr_value = calculate_arr_value(frequency, num_samples)
+        if arr_value is None:
+            return
 
+        print(f"Sending ARR value: {arr_value}")
+        print(f"Sending sample size: {num_samples}")
+
+        packet1_data = create_packet1(num_samples, arr_value)
+        time.sleep(1)
+        response1 = send_and_receive_data(ser, packet1_data)
+        if response1 is not None:
+            print("Received response data:", response1)
+        else:
+            print("Received invalid response")
+
+        lut_values = generate_sine_wave_lut(num_samples)
+        if lut_values is not None:
+            print("Generated LUT values:", lut_values)
+            packet2_data = create_packet2(lut_values)
+            time.sleep(1)
+            response2 = send_and_receive_data(ser, packet2_data)
+            if response2 is not None:
+                print("Received response data:", response2)
+            else:
+                print("Received invalid response")
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+    finally:
+        ser.close()
 
 if __name__ == "__main__":
     main()
-
